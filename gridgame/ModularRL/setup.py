a4fb9d1b241ca92d8a91ad6c455fd72b96308cc0
Tyler <tyleringebrand@gmail.com>
2023-04-04 10:39:35 -0500

    fixing modularRL

--- gridgame/ModularRL/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(name='ModularRL',
-      version='1.0',
-      description='A package for modular RL algorithm creation',
-      author='Tyler Ingebrand',
-      packages=['src'],
-     )
